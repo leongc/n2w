a42337c3c39aa6f7d17145117764deb0c6673290
Cheng Leong <cleong@netspend.com>
2010-05-07 01:00:26 -0500

    only use Strings, no more int math

--- src/org/asm/n2w/Converter.java
+++ src/org/asm/n2w/Converter.java
@@ -2,80 +2,6 @@ package org.asm.n2w;
 
 public class Converter {
 
-	public String convert(int i) {
-		if (i < 0) {
-			return "negative " + convert(-i);
-		} else if (i < 20) {
-			return convertOneToTwenty(i);
-		} else if (i < 100) {
-			int t = i / 10; // integer division gets tens digit
-			String tens = convertTens(t);
-			int u = i % 10; // modulus gets units digit
-			if (u == 0) {
-				return tens;
-			}
-			return tens + '-' + convertOneToTwenty(u);
-		} else if (i < 1000) {
-			int h = i / 100; // integer division gets hundreds digit
-			String hundreds = convertOneToTwenty(h) + " hundred";
-			int r = i % 100; // modulus gets tens and units
-			if (r == 0) {
-				return hundreds;
-			}
-			return hundreds + ' ' + convert(r);
-		} else if (i < 1000000) {
-			int th = i / 1000; // integer division gets thousands
-			String thousands = convert(th) + " thousand";
-			int r = i % 1000; // modulus gets hundreds, tens, and units
-			if (r == 0) {
-				return thousands;
-			}
-			return thousands + ' ' + convert(r);
-		}
-		throw new NumberFormatException("Cannot convert above 999,999");
-	}
-
-	private String convertOneToTwenty(int i) {
-		switch (i) {
-			case 0 : return "zero";
-			case 1 : return "one";
-			case 2 : return "two";
-			case 3 : return "three";
-			case 4 : return "four";
-			case 5 : return "five";
-			case 6 : return "six";
-			case 7 : return "seven";
-			case 8 : return "eight";
-			case 9 : return "nine";
-			case 10 : return "ten";
-			case 11 : return "eleven";
-			case 12 : return "twelve";
-			case 13 : return "thirteen";
-			case 15 : return "fifteen"; 
-			case 18 : return "eighteen";
-			
-			case 14 : 
-			case 16 :
-			case 17 :
-			case 19 : return convertOneToTwenty(i-10)+"teen";
-		}
-		throw new NumberFormatException(i + " is not a digit below twenty");
-	}
-	
-	private String convertTens(int i) {
-		switch (i) {
-			case 2 : return "twenty";
-			case 3 : return "thirty";
-			case 4 : return "forty";
-			case 5 : return "fifty";
-			case 6 : return "sixty";
-			case 7 : return "seventy";
-			case 8 : return "eighty";
-			case 9 : return "ninety";
-		}
-		throw new NumberFormatException("Unable to convert tens");
-	}
-
 	public String convert(String s) {
 		s = s.trim();
 		if (s.charAt(0) == '-') {
@@ -90,7 +16,6 @@ public class Converter {
 			, 0
 			, convertUpToThreeDigits(s.substring(lastTripletIndex)));
 	}
-
 
 	private String convertTriplets(String prefixDigits, int tripletsConverted,
 			String suffixWords) {
@@ -157,7 +82,6 @@ public class Converter {
 		String tens = convertTens(s.charAt(0));
 		return units.isEmpty() ? tens : tens + '-' + units;
 	}
-
 
 	private String convertTens(char tensDigit) {
 		switch (tensDigit) {
--- src/org/asm/n2w/ConverterTest.java
+++ src/org/asm/n2w/ConverterTest.java
@@ -15,78 +15,76 @@ public class ConverterTest {
 	}
 	@Test
 	public void uniques() {
-		assertConvert("one", 1);
-		assertConvert("two", 2);
-		assertConvert("three", 3);
-		assertConvert("four", 4);
-		assertConvert("five", 5);
-		assertConvert("six", 6);
-		assertConvert("seven", 7);
-		assertConvert("eight", 8);
-		assertConvert("nine", 9);
-		assertConvert("ten", 10);
-		assertConvert("eleven", 11);
-		assertConvert("twelve", 12);
-		assertConvert("thirteen", 13);
-		assertConvert("fifteen", 15);
-		assertConvert("eighteen", 18);
-		assertConvert("zero", 0);
+		assertConvert("zero", "0");
+		assertConvert("one", "1");
+		assertConvert("two", "2");
+		assertConvert("three", "3");
+		assertConvert("four", "4");
+		assertConvert("five", "5");
+		assertConvert("six", "6");
+		assertConvert("seven", "7");
+		assertConvert("eight", "8");
+		assertConvert("nine", "9");
+		assertConvert("ten", "10");
+		assertConvert("eleven", "11");
+		assertConvert("twelve", "12");
+		assertConvert("thirteen", "13");
+		assertConvert("fifteen", "15");
+		assertConvert("eighteen", "18");
 	}
 	@Test
 	public void teens() {
-		assertConvert("fourteen", 14);
-		assertConvert("sixteen", 16);
-		assertConvert("seventeen", 17);
-		assertConvert("nineteen", 19);
+		assertConvert("fourteen", "14");
+		assertConvert("sixteen", "16");
+		assertConvert("seventeen", "17");
+		assertConvert("nineteen", "19");
 	}
 	@Test
 	public void tens() {
-		assertConvert("ten", 10);
-		assertConvert("twenty", 20);
-		assertConvert("thirty", 30);
-		assertConvert("forty", 40);
-		assertConvert("fifty", 50);
-		assertConvert("sixty", 60);
-		assertConvert("seventy", 70);
-		assertConvert("eighty", 80);
-		assertConvert("ninety", 90);
+		assertConvert("ten", "10");
+		assertConvert("twenty", "20");
+		assertConvert("thirty", "30");
+		assertConvert("forty", "40");
+		assertConvert("fifty", "50");
+		assertConvert("sixty", "60");
+		assertConvert("seventy", "70");
+		assertConvert("eighty", "80");
+		assertConvert("ninety", "90");
 	}
 	@Test
 	public void doubleDigits() {
-		assertConvert("fifty-five", 55);
-		assertConvert("ninety-nine", 99);
+		assertConvert("fifty-five", "55");
+		assertConvert("ninety-nine", "99");
 	}
 	@Test
 	public void tripleDigits() {
-		assertConvert("one hundred", 100);
-		assertConvert("five hundred one", 501);
-		assertConvert("nine hundred eighty-seven", 987);
+		assertConvert("one hundred", "100");
+		assertConvert("five hundred one", "501");
+		assertConvert("nine hundred eighty-seven", "987");
 	}
 	@Test
 	public void thousands() {
-		assertConvert("one thousand", 1000);
-		assertConvert("two thousand one", 2001);
-		assertConvert("sixty-four thousand", 64000);
-		assertConvert("nine hundred eighty-seven thousand six hundred fifty-four", 987654);
+		assertConvert("one thousand", "1000");
+		assertConvert("two thousand one", "2001");
+		assertConvert("sixty-four thousand", "64000");
+		assertConvert("nine hundred eighty-seven thousand six hundred fifty-four", "987654");
 	}
 	@Test
 	public void negatives() {
-		assertConvert("negative sixty-nine", -69);
+		assertConvert("negative sixty-nine", "-69");
 	}
 	@Test
 	public void largeNumbers() {
 		assertConvert("four trillion one", "4000000000001");
-		assertConvert("nine billion eight hundred seventy-six million five hundred forty-three thousand two hundred ten", "9876543210");
-		assertConvert("nine nonillion eight octillion seven septillion six sextillion five quintillion four quadrillion three trillion two billion one million", "9008007006005004003002001000000");
+		assertConvert("nine billion eight hundred seventy-six million " +
+				"five hundred forty-three thousand two hundred ten"
+				, "9876543210");
+		assertConvert("nine nonillion eight octillion seven septillion six sextillion " +
+				"five quintillion four quadrillion three trillion two billion one million"
+				, "9008007006005004003002001000000");
 		assertConvert("one decillion fifty-five million", "1000000000000000000000000055000000");
 	}
 	
-	private void assertConvert(String expected, int i) {
-		Assert.assertEquals(expected, c.convert(i));
-		assertConvert(expected, Integer.toString(i));
-//		assertConvert(expected, '0' + Integer.toString(i));
-		assertConvert("negative " + expected, '-' + Integer.toString(i));
-	}
 	private void assertConvert(String expected, String s) {
 		Assert.assertEquals(expected, c.convert(s));
 	}
