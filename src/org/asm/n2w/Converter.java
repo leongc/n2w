a89a30f1c17fd6c0e3c80a40640874f3ea31c915
Cheng Leong <cleong@netspend.com>
2010-05-06 22:17:21 -0500

    zero to ninety-nine

--- src/org/asm/n2w/Converter.java
+++ src/org/asm/n2w/Converter.java
@@ -29,6 +29,23 @@ public class Converter {
 	}
 	
 	public String convert(int i) {
+		if (i < 0) {
+			throw new NumberFormatException("Cannot convert negative numbers");
+		} else if (i < 20) {
+			return convertZeroToTwenty(i);
+		} else if (i < 100) {
+			int t = i / 10; // integer division gets tens digit
+			String tens = convertTens(t);
+			int u = i % 10; // modulus gets units digit
+			if (u == 0) {
+				return tens;
+			}
+			return tens + '-' + convertZeroToTwenty(u);
+		}
+		throw new NumberFormatException("Cannot convert above 99");
+	}
+
+	private String convertZeroToTwenty(int i) {
 		switch (i) {
 			case 0 : return "zero";
 			case 1 : return "one";
@@ -40,8 +57,34 @@ public class Converter {
 			case 7 : return "seven";
 			case 8 : return "eight";
 			case 9 : return "nine";
-			default: return "ERROR";
+			case 10 : return "ten";
+			case 11 : return "eleven";
+			case 12 : return "twelve";
+			case 13 : return "thirteen";
+			case 15 : return "fifteen"; 
+			case 18 : return "eighteen";
+			
+			case 14 : 
+			case 16 :
+			case 17 :
+			case 19 : return convertZeroToTwenty(i-10)+"teen";
+			
+			default: throw new NumberFormatException(i + " is not a digit below twenty");
 		}
 	}
-
+	
+	private String convertTens(int i) {
+		switch (i) {
+			case 2 : return "twenty";
+			case 3 : return "thirty";
+			case 4 : return "forty";
+			case 5 : return "fifty";
+			case 6 : return "sixty";
+			case 7 : return "seventy";
+			case 8 : return "eighty";
+			case 9 : return "ninety";
+		}
+		throw new NumberFormatException("Unable to convert tens");
+	}
+	
 }
--- src/org/asm/n2w/ConverterTest.java
+++ src/org/asm/n2w/ConverterTest.java
@@ -14,7 +14,7 @@ public class ConverterTest {
 		c = new Converter();
 	}
 	@Test
-	public void digits() {
+	public void uniques() {
 		Assert.assertEquals("zero", c.convert(0));
 		Assert.assertEquals("one", c.convert(1));
 		Assert.assertEquals("two", c.convert(2));
@@ -25,6 +25,36 @@ public class ConverterTest {
 		Assert.assertEquals("seven", c.convert(7));
 		Assert.assertEquals("eight", c.convert(8));
 		Assert.assertEquals("nine", c.convert(9));
+		Assert.assertEquals("ten", c.convert(10));
+		Assert.assertEquals("eleven", c.convert(11));
+		Assert.assertEquals("twelve", c.convert(12));
+		Assert.assertEquals("thirteen", c.convert(13));
+		Assert.assertEquals("fifteen", c.convert(15));
+		Assert.assertEquals("eighteen", c.convert(18));
+	}
+	@Test
+	public void teens() {
+		Assert.assertEquals("fourteen", c.convert(14));
+		Assert.assertEquals("sixteen", c.convert(16));
+		Assert.assertEquals("seventeen", c.convert(17));
+		Assert.assertEquals("nineteen", c.convert(19));
+	}
+	@Test
+	public void tens() {
+		Assert.assertEquals("ten", c.convert(10));
+		Assert.assertEquals("twenty", c.convert(20));
+		Assert.assertEquals("thirty", c.convert(30));
+		Assert.assertEquals("forty", c.convert(40));
+		Assert.assertEquals("fifty", c.convert(50));
+		Assert.assertEquals("sixty", c.convert(60));
+		Assert.assertEquals("seventy", c.convert(70));
+		Assert.assertEquals("eighty", c.convert(80));
+		Assert.assertEquals("ninety", c.convert(90));
+	}
+	@Test
+	public void doubleDigits() {
+		Assert.assertEquals("fifty-five", c.convert(55));
+		Assert.assertEquals("ninety-nine", c.convert(99));
 	}
 	
 }
